e9f5876de91073d95827856bba12b3e4196f45ab
Masha Zavolnyuk <mashazavolnyuk@yandex.ru>
2016-07-08 22:41:02 +0300

    added behaviour button

--- EffectStudio/app/src/main/java/com/example/darkmaleficent/effectstudio/MainActivity.java
+++ EffectStudio/app/src/main/java/com/example/darkmaleficent/effectstudio/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.darkmaleficent.effectstudio;
 
+import android.Manifest;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -10,9 +17,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.vk.sdk.VKScope;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener,INavigation {
+        implements NavigationView.OnNavigationItemSelectedListener,INavigation,IMenuImageGallery {
+
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private static final int CAMERA_REQUEST = 2;
+    private String []scope=new String[]{VKScope.WALL,VKScope.PHOTOS};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,37 +43,87 @@ public class MainActivity extends AppCompatActivity
         toggle.syncState();
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        toGridView();
     }
 
 
 
     @Override
     public void toGridView() {
-
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        FragmentGridView fragment = new FragmentGridView();
+        ft.add(R.id.maincontainer, fragment,"galllery");
+        ft.addToBackStack("galllery");
+        ft.commit();
     }
 
     @Override
     public void toModifyImage(Bitmap image) {
-
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        FragmentWorkingImage fragment = new FragmentWorkingImage();
+        Bundle bundle = new Bundle();
+        bundle.putParcelable("image", image);
+        fragment.setArguments(bundle);
+        ft.replace(R.id.maincontainer, fragment,"modify");
+        ft.addToBackStack("modify");
+        ft.commit();
     }
 
     @Override
     public void loadImagefromGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
 
     }
 
     @Override
     public void loadImagefromCamera() {
+        // Check permission for CAMERA
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Check Permissions Now
+            // Callback onRequestPermissionsResult interceptado na Activity MainActivity
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},
+                    MainActivity.CAMERA_REQUEST);
+        } else {
+            // permission has been granted, continue as usual
+            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(cameraIntent, CAMERA_REQUEST);
+        }
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri uri = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                ImageManagerLoader.getInstance().addImage(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            ImageManagerLoader.getInstance().addImage(photo);
+            toGridView();
+        }
+    }
+
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
+        toGridView();
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        if (drawer.isDrawerOpen(GravityCompat.START)) {
+//            drawer.closeDrawer(GravityCompat.START);
+//        } else {
+//            super.onBackPressed();
+//        }
     }
 
     @Override
@@ -105,5 +171,14 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void createMenu(int position, View v) {
+        ImageManagerLoader.getInstance().setWorkingPosition(position);
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        DialogMenuImageFromGallery fragment = new DialogMenuImageFromGallery();
+        ft.add(R.id.maincontainer, fragment,"menu");
+        ft.commit();
     }
 }
